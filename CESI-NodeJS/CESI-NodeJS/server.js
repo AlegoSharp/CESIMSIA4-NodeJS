d3da59042f7b18edfaca5a10e76fadf456bd05ec
Quentin LE BRUN <quentin.le-brun@grenat-solution.fr>
2020-11-30 10:55:49 +0100

    express js

--- CESI-NodeJS/CESI-NodeJS/server.js
+++ CESI-NodeJS/CESI-NodeJS/server.js
@@ -1,8 +1,23 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const express = require('express')
+const app = express()
+const port = 3000
+
+var beerList = require('./beers/beers.json');
+
+
+app.get('/beers', function (req, res) {
+    console.log('Received request for beers from', req.ip)
+    res.json(beerList);
+});
+
+app.get('/beer/:beerId', function (req, res) {
+    console.log('Received request for ' + req.param('beerId') + ' from', req.ip)
+    var beerDetails = require('./beers/' + req.param('beerId') + '.json');
+    res.json(beerDetails);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
